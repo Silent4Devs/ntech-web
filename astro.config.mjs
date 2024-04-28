f5edc4d13d6df0c50900a77b87d89425c1ec97be
Beni Mejia <71240244+BenitoLopez12@users.noreply.github.com>
2024-04-28 14:24:15 -0600

    ingles version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https//ntechnology.dev",
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,37 @@
+export const languages = {
+    es: 'Español',
+    en: 'English',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    es: {
+        'nav.servicios': 'Servicios',
+        'nav.nosotros': 'Nosotros',
+        'nav.porque': 'Por qué Ntechnology',
+        'nav.blog': 'Blog',
+        'nav.contacto': 'Contacto',
+
+        'serv.itopsAuto' : 'ITOps Autónomo',
+        'serv.itops' : 'ITOPS',
+        'serv.nubePublica' : 'Nube Pública',
+        'serv.nubePrivada' : 'Nube Privada',
+        'serv.dataMl' : 'Data y ML',
+        'serv.industria' : 'Soluciones de Industria',
+    },
+    en: {
+        'nav.servicios': 'Services',
+        'nav.nosotros': 'Us',
+        'nav.porque': 'Why Ntechnology',
+        'nav.blog': 'Blog',
+        'nav.contacto': 'Contact',
+
+        'serv.itopsAuto' : 'Autonomous ITOps',
+        'serv.itops' : 'ITOps',
+        'serv.nubePublica' : 'Public Cloud',
+        'serv.nubePrivada' : 'Private Cloud',
+        'serv.dataMl' : 'Data and ML',
+        'serv.industria' : 'Industrial Monitoring',
+    },
+  } as const;
