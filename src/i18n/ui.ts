3afd3f5e4f31d8cae9fc6bda020f7bcc6ddb42b4
BenitoLM <benito.lopez.tecno@gmail.com>
2024-04-29 19:01:45 -0600

    lang web

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,37 +1,46 @@
 export const languages = {
-    es: 'Español',
-    en: 'English',
-  };
+  es: 'Español',
+  en: 'English',
+};
   
-  export const defaultLang = 'en';
+export const defaultLang = 'es';
   
-  export const ui = {
-    es: {
-        'nav.servicios': 'Servicios',
-        'nav.nosotros': 'Nosotros',
-        'nav.porque': 'Por qué Ntechnology',
-        'nav.blog': 'Blog',
-        'nav.contacto': 'Contacto',
+export const ui = {
+  
+  en: {
+    // header
+    'nav.servicios': 'Services',
+    'nav.nosotros': 'Us',
+    'nav.porque': 'Why Ntechnology',
+    'nav.blog': 'Blog',
+    'nav.contacto': 'Contact',
 
-        'serv.itopsAuto' : 'ITOps Autónomo',
-        'serv.itops' : 'ITOPS',
-        'serv.nubePublica' : 'Nube Pública',
-        'serv.nubePrivada' : 'Nube Privada',
-        'serv.dataMl' : 'Data y ML',
-        'serv.industria' : 'Soluciones de Industria',
-    },
-    en: {
-        'nav.servicios': 'Services',
-        'nav.nosotros': 'Us',
-        'nav.porque': 'Why Ntechnology',
-        'nav.blog': 'Blog',
-        'nav.contacto': 'Contact',
+    // servicios
+    'serv.itopsAuto' : 'Autonomous ITOps',
+    'serv.itops' : 'ITOps',
+    'serv.nubePublica' : 'Public Cloud',
+    'serv.nubePrivada' : 'Private Cloud',
+    'serv.dataMl' : 'Data and ML',
+    'serv.industria' : 'Industrial Monitoring',
 
-        'serv.itopsAuto' : 'Autonomous ITOps',
-        'serv.itops' : 'ITOps',
-        'serv.nubePublica' : 'Public Cloud',
-        'serv.nubePrivada' : 'Private Cloud',
-        'serv.dataMl' : 'Data and ML',
-        'serv.industria' : 'Industrial Monitoring',
-    },
-  } as const;
+    'btn.inicio' : 'Book a free consultation',
+  },
+  es: {
+    // header
+    'nav.servicios': 'Servicios',
+    'nav.nosotros': 'Nosotros',
+    'nav.porque': 'Por qué Ntechnology',
+    'nav.blog': 'Blog',
+    'nav.contacto': 'Contacto',
+
+    // servicios
+    'serv.itopsAuto' : 'ITOps Autónomo',
+    'serv.itops' : 'ITOPS',
+    'serv.nubePublica' : 'Nube Pública',
+    'serv.nubePrivada' : 'Nube Privada',
+    'serv.dataMl' : 'Data y ML',
+    'serv.industria' : 'Soluciones de Industria',
+
+    'btn.inicio' : 'Reserva una Demo',
+  },
+} as const;
